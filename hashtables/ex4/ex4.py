6002bb42b7fa7f313375ec079aad515bb8d3eee6
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-08-07 12:07:00 -0700

    solve ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,10 +2,26 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    # create dict
+    mydict={}
+    # create result
+    result = []
+    # populate dict
+    for i in range(len(a)):
+        #  filter out negatives
+        if a[i] < 0:
+            # change to positive
+            mydict[abs(a[i])] = i
+    # list my dict
+    keylist = list(mydict.keys())
+    # compare and append
+    for item in a:
+        if item in keylist:
+            result.append(item)
+    
     return result
 
+    
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
