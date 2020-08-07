cf8939b61c94f2c4c18e08891adde82c41ed880d
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-08-07 11:10:25 -0700

    solve ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,6 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
     # create a dict to hold onto tickets
     mydict={}
     
@@ -18,31 +15,15 @@ def reconstruct_trip(tickets, length):
         mydict[ticket.source] = ticket.destination
 
     route = []
-    # add the start
-    # route.append(mydict['NONE'])
-    # loop thru dict searching for next value by the keys
-    # base case
-    for ticket in tickets:
-            if ticket.destination is 'NONE':
-                    return route
-            # check for the start
-            if ticket.source is 'NONE':
-                route.append(ticket.destination)
-            # accumulate destinations and increment counter
-            if route[counter] in mydict.keys():
-                
-                route.append(mydict[route[counter]])
-                counter += 1
-                # print(route)
-    # return route    
+    count = 0
+     # add the start
+    place = mydict['NONE']
+    route.append(place)
+    while len(route) < length:
+        # accumulate destinations
+        next = mydict[route[count]]
+        route.append(next)
+        count += 1
+    return route 
     
-
-    
-
-ticket_1 = Ticket("NONE", "PDX")
-ticket_2 = Ticket("PDX", "DCA")
-ticket_3 = Ticket("DCA", "NONE")
-
-tickets = [ticket_1, ticket_2, ticket_3]
-reconstruct_trip(tickets, 3)
         
